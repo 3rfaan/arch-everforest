e79ab3e560bf4b0158fde406756603e0ed140308
3rfaan <28968837+3rfaan@users.noreply.github.com>
2024-05-31 20:51:04 +0200

    Added python formatter black to conform

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -2,7 +2,8 @@ require("conform").setup({
     formatters_by_ft = {
         markdown = { "prettier" },
         ["*"] = { "codespell" },
-        ["_"] = { "trim_whitespace" }
+        ["_"] = { "trim_whitespace" },
+        python = { "black" }
     },
     format_on_save = {
         lsp_fallback = true,
